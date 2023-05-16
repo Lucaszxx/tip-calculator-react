df831adf1d5858eae2ca54e89748593add0de9fa
Lucaszxx <lucasd476@gmail.com>
2023-05-16 19:44:49 -0300

    implementação e adicão da pasta images

--- react-tip-calculator/src/components/Calculadora.jsx
+++ react-tip-calculator/src/components/Calculadora.jsx
@@ -1,7 +1,52 @@
 export function Calculadora() {
-    return(
+    return (
         <main>
-            <h1>Componente calculadora</h1>
+            <form action="">
+                <div className="bill">
+                    <label htmlFor="">Conta</label>
+                    <div className="input-box">
+                        <input type="number" id="bill" placeholder="0" />
+                    </div>
+                </div>
+                <div className="tip-percentage">
+                    <label htmlFor="">Selecione a porcentagem %</label>
+                    <div>
+                        <input type="button" value="5%" />
+                        <input type="button" value="10%" />
+                        <input type="button" value="15%" />
+                        <input type="button" value="25%" />
+                        <input type="button" value="50%" />
+                        <input type="number" id="custom" placeholder="Outra" />
+                    </div>
+                </div>
+
+                <div className="people">
+                    <label htmlFor="">Número de pessoas</label>
+                    <div className="input-box">
+                        <input type="number" id="people" placeholder="0"/>
+                    </div>
+                </div>
+            </form>
+
+            <section>
+                <div className="results">
+                    <div className="tip-amount">
+                        <p>
+                            Gorjeta <br />
+                            <span>/ pessoa</span>
+                        </p>
+                        <p>R$0.00</p>
+                    </div>
+                </div>
+                <div className="total">
+                    <p>
+                        Total <br />
+                        <span>/ pessoa</span>
+                    </p>
+                    <p>R$0.00</p>
+                </div>
+                <button>Limpar</button>
+            </section>
         </main>
     )
 }
